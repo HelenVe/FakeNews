971bb6b02f3beeeb1868c1cb74b7e33470ff85d7
Olympia <oaxelou@uth.gr>
2020-05-01 22:17:27 +0300

    site ready! (input: the url)

--- django-stuff/env/src/fakenews/CheckIfFakeNews/views.py
+++ django-stuff/env/src/fakenews/CheckIfFakeNews/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 import validators
@@ -5,59 +6,99 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import json
 import tensorflow as tf
+from newspaper import Article
+from bs4 import BeautifulSoup
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-max_features=4500
+max_features = 4500
 tokenizer = Tokenizer(num_words=max_features, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' ')
+
 
 # Create your views here.
 
+
 def home(request):
-	context={}
-	return render(request, "home.html", context) # {}: context variables
+    context = {}
+    return render(request, "home.html", context)  # {}: context variables
 
 
 def result(request):
-	if 'title' in request.GET and 'author' in request.GET and 'article' in request.GET:
-		print("Found title:",request.GET.get('title'))
-		print("Found author:",request.GET.get('author'))
-		print("Found article:",request.GET.get('article'))
-	else:
-		if 'title' not in request.GET:
-			print("TITLE NOT EXISTING")
-		if 'author' not in request.GET:
-			print("AUTHOR NOT EXISTING")
-		if 'article' not in request.GET:
-			print("ARTICLE NOT EXISTING")
-		# print("SHOULD NEVER REACH THIS. title or author or article not in parameters")
-		return HttpResponse("<h1>No data to display!</h1>")
+    if 'url' in request.GET:
+        url = request.GET.get('url')
+        page = Article(url)
 
-	title=request.GET.get('title')
-	author=request.GET.get('author')
-	article=request.GET.get('article')
+        html = page.html
+        soup = BeautifulSoup(html, 'lxml')
+        title = soup.title
+        page.download()
+        page.parse()
+        author = page.authors
+        ', '.join(author)
 
-	###################################################################################################################################################################
-	# Preparing the test dataset
+        article = page.text
+        print(title)
+        print(author)
+        print(article)
 
-	test_data=pd.DataFrame([[title,author,article]],columns=['title','author','text'])
+    else:
+        return HttpResponse("<h1>No data to display!</h1>")
 
-	# Filling the Missing values
-	test_data = test_data.fillna(' ')
-	tokenizer.fit_on_texts(texts = test_data['text'])
-	test_text = tokenizer.texts_to_sequences(texts = test_data['text'])
-	test_text = pad_sequences(sequences = test_text, maxlen = max_features, padding = 'pre')
+    # if 'title' in request.GET and 'author' in request.GET and 'article' in request.GET:
+    # 	print("Found title:",request.GET.get('title'))
+    # 	print("Found author:",request.GET.get('author'))
+    # 	print("Found article:",request.GET.get('article'))
+    # else:
+    # 	if 'title' not in request.GET:
+    # 		print("TITLE NOT EXISTING")
+    # 	if 'author' not in request.GET:
+    # 		print("AUTHOR NOT EXISTING")
+    # 	if 'article' not in request.GET:
+    # 		print("ARTICLE NOT EXISTING")
+    # 	# print("SHOULD NEVER REACH THIS. title or author or article not in parameters")
+    # 	return HttpResponse("<h1>No data to display!</h1>")
 
-	###################################################################################################################################################################
+    # title=request.GET.get('title')
+    # author = request.GET.get('author')
+    # article = request.GET.get('article')
 
-	# Prediction
-	fakeNews=False
-	gru_model = tf.keras.models.load_model(os.path.join(os.path.join(settings.BASE_DIR,'static'),'model.h5'))
-	gru_prediction = gru_model.predict(test_text)
+    # too small
+    if len(article) < 50:
+        return HttpResponse("<h1>Text too small! </h1>")
+    if len(author) == 0:
+        author = ["Not found"]
+    fake_percent = []
+    if len(author) != 0:
+        path = os.path.join(os.path.join(settings.BASE_DIR, 'static'), 'dict.json')
+        with open(path) as json_file:
+            dictionary = json.load(json_file)
+        for a in author:
+            if a in dictionary:
+                fake_percent.append(dictionary[a])
 
-	print("scores of the regression:", gru_prediction)
+    ###################################################################################################################################################################
+    # Preparing the test dataset
 
-	context={'title':title, 'author':author, 'article':article, 'isFake':True, 'percentage':gru_prediction[0][0]*100}
-	return render(request, "result.html", context) # {}: context variables
+    test_data = pd.DataFrame([[author, article]], columns=['author', 'text'])
 
+    # Filling the Missing values
+    test_data = test_data.fillna(' ')
+    tokenizer.fit_on_texts(texts=test_data['text'])
+    test_text = tokenizer.texts_to_sequences(texts=test_data['text'])
+    test_text = pad_sequences(sequences=test_text, maxlen=max_features, padding='pre')
+
+    ###################################################################################################################################################################
+
+    # Prediction
+
+    model = tf.keras.models.load_model(os.path.join(os.path.join(settings.BASE_DIR, 'static'), 'model.h5'))
+    prediction = model.predict(test_text)
+
+    print("scores of the regression:", prediction)
+    pred = round(prediction[0][0], 2)
+    print(pred)
+
+    context = {'author': author, 'article': article, 'percentage': pred * 100, 'fake_percent': fake_percent}
+    return render(request, "result.html", context)  # {}: context variables
